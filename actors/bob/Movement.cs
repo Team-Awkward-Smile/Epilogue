884675b9ff9d9c2dcedafd66480cba9e82d7d7a2
vitormaduro <vitormaduro006@gmail.com>
2023-05-27 17:24:59 -0300

    29 - Partial implementation of movement system

---
+++ actors/bob/Movement.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+
+namespace Actors.Hestmor;
+public partial class Movement : CharacterBody2D
+{
+	public override void _Input(InputEvent @event)
+	{
+		if(Input.IsActionPressed("move_left"))
+		{
+			
+		}
+	}
+}
