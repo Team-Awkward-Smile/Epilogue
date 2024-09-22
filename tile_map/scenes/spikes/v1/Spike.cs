a6e8497f7fab1d14fcffdf2157f2f7274825f938
Hhk187 <2003hasnin@gmail.com>
2024-09-22 18:18:38 +0200

    added: - Spikes now do damage - they twitch when player gets near

--- tile_map/scenes/spikes/v1/Spike.cs
+++ tile_map/scenes/spikes/v1/Spike.cs
@@ -2,15 +2,24 @@ using Epilogue.Nodes;
 using Godot;
 using System;
 
-
+[Icon("res://tile_map/scenes/spikes/v1/Spikes2.png")]
 public partial class Spike : RigidBody2D
 {
-	private RayCast2D _rayCast2D;
+	private RayCast2D _trigger;
+	private RayCast2D _leftWarning;
+	private RayCast2D _rightWarning;
+
+	private AnimationPlayer _animationPlayer;
 	private bool _dropped = false;
+	private bool _warned = false;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		_rayCast2D = GetNode<RayCast2D>("RayCast2D");
+		_trigger = GetNode<RayCast2D>("Trigger");
+		_leftWarning = GetNode<RayCast2D>("LeftWarning");
+		_rightWarning = GetNode<RayCast2D>("RightWarning");
+
+		_animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
 		
 		TileMap levelTileMap = (TileMap)GetParent();
 		levelTileMap.AddToLstSpike(this);
@@ -19,7 +28,7 @@ public partial class Spike : RigidBody2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override async void _Process(double delta)
 	{
-		if (_rayCast2D.GetCollider() is Player && !_dropped)
+		if (_trigger.GetCollider() is Player && !_dropped)
 		{
 			_dropped = true;
 			GravityScale = 1;
@@ -27,11 +36,19 @@ public partial class Spike : RigidBody2D
 			QueueFree();
 		}
 
+		if ((_leftWarning.GetCollider() is Player || _rightWarning.GetCollider() is Player) && !_warned)
+		{
+			_animationPlayer.Play("twitch");
+			_warned = true;
+		}
+
 	}	
 
 	public void setRaycatDistance(int distance)
 	{
-		Vector2 newDistance = new Vector2(_rayCast2D.TargetPosition.X, distance * 36);
-		_rayCast2D.TargetPosition = newDistance;
+		Vector2 newDistance = new Vector2(0, distance * 36);
+		_trigger.TargetPosition = newDistance;
+		_leftWarning.TargetPosition = newDistance;
+		_rightWarning.TargetPosition = newDistance;
 	}
 }
---
+++ tile_map/scenes/spikes/v1/spike_hurtbox.cs
@@ -0,0 +1,27 @@
+using Epilogue.Global.Enums;
+using Epilogue.Nodes;
+using Godot;
+using System;
+public partial class spike_hurtbox : HitBox
+{	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+
+	}
+
+	public void _on_area_entered(Area2D area2D)
+	{
+		if (area2D is HurtBox hurtBox)
+		{
+			hurtBox.OnHit(this);
+		}
+		GD.Print(area2D);
+		Monitoring = false;	
+	}
+}
