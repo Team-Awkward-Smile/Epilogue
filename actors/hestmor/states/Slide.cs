89de70f39ff5d6199f2dd131231354df80e179b3
Hhk187 <2003hasnin@gmail.com>
2025-01-24 18:23:33 +0100

    fixed bugs

--- actors/hestmor/states/Slide.cs
+++ actors/hestmor/states/Slide.cs
@@ -31,9 +31,7 @@ public partial class Slide : State
 	private ShapeCast2D _slideShapeCast2D;
 	private RayCast2D _slideEndRayCast2D;
 	private FootstepManager _footstepManager;
-
-	private FootstepManager _footstepManager;
-
+	
 	/// <summary>
 	/// 	State that allows Hestmor to perform slides
 	/// </summary>
