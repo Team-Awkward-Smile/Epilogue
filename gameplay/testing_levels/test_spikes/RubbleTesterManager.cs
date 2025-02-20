2c7a1c07386cf2de0f68e7c7960d6c3dc9213f8b
Hhk187 <2003hasnin@gmail.com>
2025-02-20 18:38:59 +0100

    Added: kill switch for low end computer, well just in case hahaha

--- gameplay/testing_levels/test_spikes/RubbleTesterManager.cs
+++ gameplay/testing_levels/test_spikes/RubbleTesterManager.cs
@@ -11,6 +11,8 @@ public partial class RubbleTesterManager : Node2D
 	[Export] private int _maxRubbles = 100;
 	private Node2D _rubbles;
 	private bool active = false;
+
+	private bool _killSwitchActive = false;
 	
 
 	// Called when the node enters the scene tree for the first time.
@@ -18,6 +20,23 @@ public partial class RubbleTesterManager : Node2D
 	{
 		_rubbles = GetNode<Node2D>("Rubbles");
 	}
+
+	public override void _Process(double delta)
+	{
+		base._Process(delta);
+
+		if (Engine.GetFramesPerSecond() < 10 && _killSwitchActive)  {
+			GetTree().Quit();
+		}	
+	}
+
+
+	public void _on_kill_switch_buffer_timeout()
+	{
+		_killSwitchActive = true;
+		GD.Print("KILL SWITCH HAS BEEN ACTIVATED");
+	}
+
 
 	/// <inheritdoc/>
 
