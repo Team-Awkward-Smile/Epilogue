994b0106065a4a8ec52791198b85ab50419525d1
hhk187 <2003hasnin@gmail.com>
2025-10-30 16:42:01 +0100

    Changes: - removed the duplicate function inside `ActorAudioPlayer.cs`

--- nodes/ActorAudioPlayer.cs
+++ nodes/ActorAudioPlayer.cs
@@ -198,27 +198,6 @@ public partial class ActorAudioPlayer : Node
 		var rng = new RandomNumberGenerator();
 		var possibleSfx = GenericSfxList.Where(sfx => sfx.Key.StartsWith(prefix));
 
-		if (!possibleSfx.Any())
-		{
-			GD.PushWarning($"No Collision SFX found for prefix [{prefix}] and Actor [{Owner.Name}]");
-
-			return;
-		}
-
-		var sfx = possibleSfx.ElementAt(rng.RandiRange(0, possibleSfx.Count() - 1)).Value;
-
-		_genericSfxPlayer.Stream = sfx;
-		_genericSfxPlayer.Play();
-	}
-	/// <summary>
-	///		Plays a random Collision SFX whose name starts with <paramref name="prefix"/>
-	/// </summary>
-	/// <param name="prefix">The prefix of the category of SFX to be played</param>
-	public void PlayRandomGenericSfx(string prefix)
-	{
-		var rng = new RandomNumberGenerator();
-		var possibleSfx = GenericSfxList.Where(sfx => sfx.Key.StartsWith(prefix));
-
 		if (!possibleSfx.Any() )
 		{
 			GD.PushWarning($"No Collision SFX found for prefix [{prefix}] and Actor [{Owner.Name}]");
