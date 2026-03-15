9548e17f70f80dba4e2d2907c432e90e1555f93b
Hhk187 <114492671+Hhk187@users.noreply.github.com>
2026-03-15 13:51:31 +0100

    fixed after merge with develop

--- actors/hestmor/MainSprite.cs
+++ actors/hestmor/MainSprite.cs
@@ -16,8 +16,8 @@ public partial class MainSprite : Sprite2D
 		{
 			ID = (int)SpriteSheetId.IdleWalk,
 			Texture = GD.Load<CompressedTexture2D>("res://actors/hestmor/sprite_sheet/hestmor_1-Sheet.png"),
-			HFrames = 8,
-			VFrames = 7,
+			HFrames = 13,
+			VFrames = 12,
 			Scale = new Vector2(1f, 1f)
 		},
 		new SpriteSheetDataDto()
